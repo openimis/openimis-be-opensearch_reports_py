78175bf1d3ffc9c1f08d24dbc20902c42487da84
sniedzielski <sniedzielski@soldevelo.com>
2024-09-20 12:07:57 +0200

    Add service to manage base synch enable/disable - opensearch

--- opensearch_reports/service.py
+++ opensearch_reports/service.py
@@ -1,5 +1,7 @@
 import logging
 
+from django_opensearch_dsl import Document
+
 from core.services import BaseService
 from core.signals import register_service_signal
 from opensearch_reports.models import OpenSearchDashboard
@@ -18,3 +20,34 @@ class OpenSearchDashboardService(BaseService):
 
     def __init__(self, user, validation_class=OpenSearchDashboardValidation):
         super().__init__(user, validation_class)
+
+
+class BaseSyncDocument(Document):
+    """
+    Base document class that controls synchronization based on the 'synch_disabled' flag.
+    All OpenSearch document classes should inherit from this class.
+    """
+    DASHBOARD_NAME = None
+
+    def is_sync_disabled(self):
+        try:
+            dashboard = OpenSearchDashboard.objects.get(name=self.DASHBOARD_NAME)
+            print(dashboard)
+            return dashboard.synch_disabled
+        except OpenSearchDashboard.DoesNotExist:
+            # If no dashboard entry, assume sync is enabled
+            return False
+
+    def save(self, **kwargs):
+        if not self.is_sync_disabled():
+            super().save(**kwargs)  # Proceed with syncing
+        else:
+            print(f"Sync is disabled for index '{self._index._name}'")
+            logger.warning(f"Sync is disabled for index '{self._index._name}'")
+
+    def delete(self, **kwargs):
+        if not self.is_sync_disabled():
+            super().delete(**kwargs)  # Proceed with deletion
+        else:
+            print(f"Sync is disabled for index '{self._index._name}'")
+            logger.warning(f"Sync is disabled for index '{self._index._name}'")
