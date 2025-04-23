6473a235e91a6a1c1015b907bd7ebe30901ce2ff
Patrick Delcroix <delcroip@gmail.com>
2025-04-23 09:00:54 +0200

    Update test_service.py

--- opensearch_reports/tests/test_service.py
+++ opensearch_reports/tests/test_service.py
@@ -6,7 +6,7 @@ from django_opensearch_dsl.documents import Document
 from django_opensearch_dsl.registries import registry
 from unittest.mock import patch
 from opensearchpy import OpenSearch
-
+from core import settings
 from opensearch_reports.models import OpenSearchDashboard
 from opensearch_reports.service import BaseSyncDocument
 
@@ -40,6 +40,7 @@ class BaseSyncDocumentTest(TestCase):
         cls.dashboard.save(username=cls.user.username)
 
     @patch.object(Document, "bulk")
+    @unittest.skipIf(not settings.OPENSEARCH_DSL_AUTOSYNC, "Skipping test because OPENSEARCH_DSL_AUTOSYNC is False")
     def test_auto_refresh_sync_enabled(self, mock_doc_bulk):
         log = MutationLog.objects.create(json_content='foobar')
 
